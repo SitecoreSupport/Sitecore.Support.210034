2eec7beb4fe58c121724d9e2579ec7cc59398a66
DK\msb <msb@sitecore.net>
2018-03-14 18:06:49 -0400

    Fix issue to update also Duration for previous page. Also part of missed EndAnalytics pipeline

--- src/Sitecore.Support.210034/Sitecore.Analytics/Pipelines/CommitSession/UpdateEndDateTime.cs
+++ src/Sitecore.Support.210034/Sitecore.Analytics/Pipelines/CommitSession/UpdateEndDateTime.cs
@@ -22,15 +22,29 @@ namespace Sitecore.Support.Sitecore.Analytics.Pipelines.CommitSession
             Assert.ArgumentNotNull(args, "args");
 
             Assert.IsNotNull(args.Session.Interaction, "args.Session.Interaction");
-
-            if (args.Session.Interaction.EndDateTime < args.Session.Interaction.GetPages().Last<IPageContext>().DateTime)
+            var lastPage = args.Session.Interaction.GetPages().Last<IPageContext>();
+            if (args.Session.Interaction.EndDateTime < lastPage.DateTime)
             {
                 // Attempt to update end date time if not done earlier in the visit.
+                SetDuration(lastPage, args.Session.Interaction);
                 int num = args.Session.Interaction.GetPages().Sum<IPageContext>((Func<IPageContext, int>)(d => d.Duration));
                 args.Session.Interaction.EndDateTime = args.Session.Interaction.StartDateTime.AddMilliseconds((double)num);
             }
         }
 
+
+        private void SetDuration(IPageContext page, CurrentInteraction interation)
+        {
+            if (page.CanSetDuration)
+            {
+                IPageContext previousPage = interation.PreviousPage;
+                if (previousPage != null)
+                {
+                    TimeSpan span = (TimeSpan)(page.DateTime - previousPage.DateTime);
+                    previousPage.Duration = (int)span.TotalMilliseconds;
+                }
+            }
+        }
         #endregion
     }
 }
